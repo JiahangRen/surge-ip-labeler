b52ecc85b91552d23830aae67b65a0654d9fd11e
Jiahang Ren <renjiahang1201@Gmail.com>
2026-07-14 22:24:17 +0800

    fix: show required module parameters

--- test/module-metadata.test.js
+++ test/module-metadata.test.js
@@ -7,7 +7,7 @@ const modulePath = new URL('../src/surge/ip-labeler.sgmodule', import.meta.url);
 test('wires Surge module arguments into the cron script invocation', async () => {
   const moduleText = await readFile(modulePath, 'utf8');
 
-  assert.match(moduleText, /^#!arguments=source_url=&upload_url=&upload_token=$/m);
+  assert.match(moduleText, /^#!arguments=source_url=REQUIRED_SOURCE_URL&upload_url=REQUIRED_UPLOAD_URL&upload_token=REQUIRED_SYNC_TOKEN$/m);
   assert.match(
     moduleText,
     /^surge-ip-labeler = .*script-path=https:\/\/WORKER\/surge-ip-labeler\.js.*argument="source_url=%source_url%&upload_url=%upload_url%&upload_token=%upload_token%"/m,
